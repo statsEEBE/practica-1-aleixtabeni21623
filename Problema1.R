b99678c2e71905122926ec087573af6710c77d2a
aleix.tabeni <aleix.tabeni@auleseebe.upc.edu>
2024-09-19 11:33:27 +0200

    Practica problema 1

--- Problema1.R
+++ Problema1.R
@@ -1,3 +1,25 @@
 #Codigo para problema 1
+mis_dades <- mtcars
+mis_dades
+qsec_intervalos <- cut(mis_dades$qsec, breaks = 4)
+qsec_intervalos
+table(qsec_intervalos)
+hist(mis_dades$qsec, breaks = 4)
+mean(mis_dades$qsec)
 
-plot(1:5)
+mis_dades$drat
+sort(mis_dades$drat)
+median(mis_dades$drat)
+quantile(mis_dades$drat, 0.25)
+quantile(mis_dades$mpg,0.18)
+ni <- table(qsec_intervalos)
+fi <- ni/sum(ni)
+Ni <- cumsum(ni)
+Fi <- cumsum(fi)
+quantile(mis_dades$cyl, 0.75)-quantile(mis_dades$cyl, 0.25)
+IQR(mis_dades$cyl)
+boxplot(mis_dades$cyl)
+#desviacio corregida tipica
+sd(mis_dades$cyl)        
+#variancia corregida
+var(mis_dades$qsec)
